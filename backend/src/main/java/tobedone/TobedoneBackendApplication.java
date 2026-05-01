f06c4e9ae40408da7e20a24235f7d8b0ba70f630
samuelPaullo <samuel.paullo@gmail.com>
2026-05-01 15:59:55 -0300

    chore: initial monorepo with backend and frontend

---
+++ backend/src/main/java/tobedone/TobedoneBackendApplication.java
@@ -0,0 +1,13 @@
+package tobedone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobedoneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TobedoneBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/tobedone/TobedoneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tobedone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobedoneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
